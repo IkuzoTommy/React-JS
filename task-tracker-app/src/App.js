841cb584e09f097edd5581b20fdf258d5a39e41b
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-08-23 16:45:53 -0400

    task tracker

---
+++ task-tracker-app/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import Button from './components/Button';
+
+function App(props) {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ task-tracker-app/src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = (color, text) => {
+  return (
+    <div>
+        <button style={{backgroundColor: color}} className='btn'>Add</button>
+    </div>
+  )
+}
+
+export default Button
