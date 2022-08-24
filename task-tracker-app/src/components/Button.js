be1c5581bb8c3d86fef67883457681bc12acd777
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-08-24 17:12:44 -0400

    task tracker

--- task-tracker-app/src/components/Button.js
+++ task-tracker-app/src/components/Button.js
@@ -1,9 +1,26 @@
-const Button = (color, text) => {
+import PropTypes from 'prop-types'
+const Button = ({color, text}) => {
+  const onClick = () => {
+    console.log('click')
+  }
   return (
     <div>
-        <button style={{backgroundColor: color}} className='btn'>Add</button>
+        <button 
+        onClick={onClick} 
+        style={{backgroundColor: color}} 
+        className='btn'>{text}</button>
     </div>
   )
+}
+
+Button.defaultProps=
+{
+  color: 'steelblue',
+}
+
+Button.propTypes ={
+  text: PropTypes.string,
+  color: PropTypes.string,
 }
 
 export default Button
--- task-tracker-app/src/components/Header.js
+++ task-tracker-app/src/components/Header.js
@@ -4,8 +4,10 @@ const Header = ({title}) => {
   return (
     <header>
         <h1 className='header'>{title}</h1>
-        <Button color='green' text='Add' />
-
+        <Button 
+        color='green' 
+        text='Add'
+        onClick = {onClick} />
     </header>
   )
 }
