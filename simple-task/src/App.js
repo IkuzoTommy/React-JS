c9b1013ae0842b58887abb2435b933cc97b93485
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-08-25 20:46:34 -0400

    task tracker update

--- simple-task/src/App.js
+++ simple-task/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
+import React, {useState}  from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+    return (
+    <>
+      <TodoList todos = {todos}/> 
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>
+        0 left todo
+      </div>
+
+    </>
   );
 }
 
